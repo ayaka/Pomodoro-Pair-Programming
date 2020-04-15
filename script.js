98bd72458c715594816131558d7822fc313cf47f
threeaces <dev.threeaces@gmail.com>
2020-04-14 21:53:11 -0700

    day 2 night work

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const timeControlBtns = document.querySelectorAll(".time-control");
 const sessionTime = document.querySelector(".session-time");
 const breakTime = document.querySelector(".break-time");
-const timer = document.querySelector(".timer");
+// !!!!!!
+// const timer = document.querySelector(".timer");
 const startBtn = document.getElementById("start");
 const pauseBtn = document.getElementById("pause");
 const resetBtn = document.getElementById("reset");
+
+// !!!!!!
+const displayMinutes = document.querySelector(".display-minutes");
+const displaySeconds = document.querySelector(".display-seconds");
 
 // initialize
 let inSession = true;
@@ -15,9 +20,11 @@ let breakMinutes = breakTime.textContent;
 let breakSeconds = 0;
 
 function updateDisplay(minutes, seconds) {
-  minutes = minutes < 10 ? "0" + minutes : minutes;
+  // !!!!!!!!
+  // minutes = minutes < 10 ? "0" + minutes : minutes;
   seconds = seconds < 10 ? "0" + seconds : seconds;
-  timer.textContent = `${minutes}:${seconds}`;
+  displayMinutes.textContent = minutes;
+  displaySeconds.textContent = seconds;
   document.title = `${minutes}:${seconds}`;
 }
 
@@ -30,7 +37,10 @@ function addMinute(element) {
 
 function subtractMinute(element) {
   let count = parseInt(element.textContent);
-  count--;
+  // !!!!!!
+  if (count > 1) {
+    count--;
+  }
   element.textContent = count;
   element === sessionTime ? (sessionMinutes = count) : (breakMinutes = count);
 }
@@ -63,37 +73,44 @@ let timerInterval;
 
 // function to start timer countdown
 function startTimer() {
- 
-    let value = timer.textContent.split('')    
-    let numArr = value.filter(num => num != ':')
-    let countdown = parseInt((numArr[0] + numArr[1]) *60) + parseInt(numArr[2] + numArr[3])
-    console.log(countdown);    
+  // let value = timer.textContent.split("");
+  // let numArr = value.filter((num) => num != ":");
+  // let countdown =
+  //   parseInt((numArr[0] + numArr[1]) * 60) + parseInt(numArr[2] + numArr[3]);
+
+  // !!!!!!!
+  let countdown =
+    parseInt(displayMinutes.textContent * 60) +
+    parseInt(displaySeconds.textContent);
   timerInterval = setInterval(function () {
-    
+    // !!!!!!!
+    countdown--;
     updateDisplay(parseInt(countdown / 60, 10), parseInt(countdown % 60, 10));
     if (paused) {
-        console.log('paused activated');
+      console.log("paused activated");
       return;
     }
-
-    if (--countdown < 0) {
+    // !!!!!!!
+    if (countdown === 0) {
       if (inSession) {
-        countdown = breakMinutes * 60;
+        // !!!!!!
+        countdown = breakMinutes * 60 + 1;
         inSession = false;
       } else {
-        countdown = sessionMinutes * 60;
+        // !!!!!!
+        countdown = sessionMinutes * 60 + 1;
         inSession = true;
       }
     }
   }, 1000);
-};
+}
 
 function timeIt() {
   paused = false;
   manageControlBtns(false);
   startBtn.removeEventListener("click", timeIt);
   startTimer();
-};
+}
 
 startBtn.addEventListener("click", timeIt);
 
@@ -103,21 +120,21 @@ resetBtn.addEventListener("click", function () {
   sessionMinutes = sessionTime.textContent;
   clearInterval(timerInterval);
   updateDisplay(sessionMinutes, sessionSeconds);
-
 });
 
-function pauseTimer(){
+function pauseTimer() {
   clearInterval(timerInterval);
-  startBtn.addEventListener('click', timeIt);
-    if(paused === true){
-        paused = false;
-      }
-      else if (paused === false){
-        paused = true;
-      }
+  startBtn.addEventListener("click", timeIt);
+
+  // !!!!!!!!!
+  // if (paused === true) {
+  //   paused = false;
+  // } else if (paused === false) {
+  //   paused = true;
+  // }
 }
 
-pauseBtn.addEventListener("click", pauseTimer) 
+pauseBtn.addEventListener("click", pauseTimer);
 
 manageControlBtns(true);
 
